b5974bc92616030c6c8d7dcd0c79f3c798e52403
elements <126486+elements@users.noreply.github.com>
2020-04-19 00:06:43 -0700

    v1.1 - Moved to config yaml file - Moved to central logging initialization - Moved old birb command to obirb - Added New birb command that pulls from imgur - Added birb clear cache to clear imgur cached result

--- bot/cogs/birb.py
+++ bot/cogs/birb.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import discord
 from discord.ext import commands
@@ -10,8 +11,10 @@ from utils.functions import func
 class Birb(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.birbImgurCache = None
+        self.birbImgurCacheTime = None
 
-    @commands.command(name="birb")
+    @commands.command(name="obirb")
     async def _birb(self, ctx):
         async with aiohttp.ClientSession() as session:
             async with session.get("https://birb.henry.fail/api") as r:
@@ -26,24 +29,34 @@ class Birb(commands.Cog):
                                                      "Couldn't reach birb.henry.fail.\nTry again later.",
                                                      "Error"))
 
-    @commands.command(name="rbirb")
-    @commands.is_owner()
-    async def _rbirb(self, ctx):
+    async def _getimgurgallery(self, tag):
         headers = {'Authorization': 'Client-ID {}'.format(self.bot.bot_config.IMGUR_TOKEN)}
-        url = f"https://api.imgur.com/3/gallery/t/birb"
+        url = f"https://api.imgur.com/3/gallery/t/{tag}"
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=headers) as r:
                 if r.status == 200:
-                    js = await r.json()
+                    return await r.json()
 
-                    imgurItem = random.choice(js["data"]["items"])
+    @commands.command(name="birb")
+    async def _rbirb(self, ctx):
+        if (self.birbImgurCache is None) or (time.time() - self.birbImgurCacheTime > 43200):
+            js = await self._getimgurgallery('birb')
+            self.birbImgurCache = js
+            self.birbImgurCacheTime = time.time()
+        else:
+            js = self.birbImgurCache
+        print(len(js["data"]["items"]))
+        imgurItem = random.choice(js["data"]["items"])
+        await ctx.send(imgurItem['link'])
 
-                    await ctx.send(imgurItem['link'])
-                else:
-                    await ctx.send(embed=func.EMaker(self, "Oops!",
-                                                     f"Couldn't reach imgur.\nTry using {ctx.prefix}birb instead",
-                                                     "Error"))
-
+    @commands.command(name="birbclearcache")
+    @commands.is_owner()
+    async def _rbirbCacheClear(self, ctx):
+        js = await self._getimgurgallery('birb')
+        self.birbImgurCache = js
+        self.birbImgurCacheTime = time.time()
+        self.bot.log.info('Birb Cache Cleared')
+        await ctx.send('**`SUCCESS`**')
 
 def setup(client):
     client.add_cog(Birb(client))
