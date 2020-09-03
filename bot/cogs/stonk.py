442c129f248d77085fa3a7ca3fde725c54da7651
Greg Butt <greg.butt@scaledagile.com>
2020-09-02 21:37:39 -0600

    Squashed commit of the following:
    commit 3ee02b1977df5a29f620e0d9866d32cbfd98985b
    Author: Greg Butt <greg.butt@scaledagile.com>
    Date:   Wed Sep 2 20:28:52 2020 -0600
    
        stonk: Add extra info, colors

--- bot/cogs/stonk.py
+++ bot/cogs/stonk.py
@@ -1,35 +1,65 @@
-import os
-
 import discord
 from discord.ext import commands
-
 
 class Stonk(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="stonk")
-    async def stonk(self, ctx, *ticker: str):
-        if len(ticker) == 1:
-            ticker = ticker[0]
+    async def stonk(self, ctx, *tickers: str):
+        try:
             await ctx.trigger_typing()
-            async with self.bot.session.get("https://query1.finance.yahoo.com/v8/finance/chart/{}".format(ticker)) as r:
-                if r.status == 200:
-                    js = await r.json()
-                    regularMarketPrice = js['chart']['result'][0]['meta']['regularMarketPrice']
-                    tickerSymbol = js['chart']['result'][0]['meta']['symbol']
-                    tickerCurrency = js['chart']['result'][0]['meta']['currency']
+            for ticker in tickers:
+                embed = await self.build_embed(ticker)
+                await ctx.send(embed=embed)
+        except Exception as ex:
+            self.bot.log.error(ex)
 
-                    em = discord.Embed(title="{} {} Price".format(tickerSymbol, tickerCurrency), color=discord.Color.green(),
-                                       url="https://finance.yahoo.com/quote/{0}?p={0}".format(ticker))
-                    em.add_field(name="Current Price", value=regularMarketPrice)
-                    await ctx.send(embed=em)
-                elif r.status == 404:
-                    await ctx.send("Error quote not found for {}".format(ticker))
+    async def build_embed(self, ticker):
+        async with self.bot.session.get("https://query1.finance.yahoo.com/v7/finance/options/{}".format(ticker)) as r:
+            embed = None
+            if r.status == 200:
+                js = await r.json()
+                if len(js["optionChain"]["result"]) > 0:
+                    embed = self.build_quote_embed(js)
                 else:
-                    await ctx.send("Error retrieving quote data for {}. Try again later.".format(ticker))
-        else:
-            await ctx.send("Error please send 1 ticker symbol. `$stonk CRM`")
+                    embed = self.build_not_found_embed(ticker)
+            elif r.status == 404:
+                embed = self.build_not_found_embed(ticker)
+            else:
+                embed = discord.Embed(title="Error retrieving quote data for {}. Try again later.".format(ticker))
+            
+            return embed
+
+    def build_not_found_embed(self, ticker):
+        return discord.Embed(title="Error quote not found for {}".format(ticker))
+
+    def build_quote_embed(self, js):
+        quoteDict = js["optionChain"]["result"][0]["quote"]
+        tickerSymbol = quoteDict["symbol"]
+        tickerCurrency = quoteDict["currency"]
+        longName = quoteDict["longName"]
+        fullExchangeName = quoteDict["fullExchangeName"]
+        regularMarketPrice = quoteDict["regularMarketPrice"]
+        regularMarketChange = round(quoteDict["regularMarketChange"], 2)
+        regularMarketChangePercent = round(quoteDict["regularMarketChangePercent"], 2)
+
+        embed = discord.Embed(
+            title="{} ({})".format(longName, tickerSymbol),
+            color=discord.Color.green(),
+            url="https://finance.yahoo.com/quote/{0}?p={0}".format(tickerSymbol),
+        )
+        # format using diff markdown co we get pretty colors
+        if regularMarketChange > 0:
+            regularMarketChange = "+{}".format(regularMarketChange)
+        if regularMarketChangePercent > 0:
+            regularMarketChangePercent = "+{}".format(regularMarketChangePercent)
+        embed.add_field(name="Price", value="```diff\n{}```".format(regularMarketPrice), inline=True)
+        embed.add_field(name="Change", value="```diff\n{}```".format(regularMarketChange), inline=True)
+        embed.add_field(name="Change %", value="```diff\n{}```".format(regularMarketChangePercent), inline=True)
+        embed.add_field(name="Exchange", value=fullExchangeName, inline=True)
+        embed.add_field(name="Currency", value=tickerCurrency, inline=True)
+        return embed
 
 def setup(client):
     client.add_cog(Stonk(client))
