2f46160b23a983d6a4dd85d6fb97ad7d1cca795e
Greg Butt <greg.butt@scaledagile.com>
2020-09-09 19:03:30 -0600

    move stonk chart to utils class

--- bot/cogs/stonk.py
+++ bot/cogs/stonk.py
@@ -1,6 +1,6 @@
-import drawSvg as draw
 import discord
 from discord.ext import commands
+from utils.stonkchart import Chart
 import io
 
 class Stonk(commands.Cog):
@@ -73,117 +73,10 @@ class Stonk(commands.Cog):
                 quotes = js["chart"]["result"][0]["indicators"]["quote"][0]["close"]
                 previousClose = js["chart"]["result"][0]["meta"]["previousClose"]
 
-                # get quote stats
-                num_quotes = len(quotes)
-                max_quote = previousClose
-                min_quote = previousClose
-                for quote in quotes:
-                    if quote is not None and quote > max_quote:
-                        max_quote = quote
-                    if quote is not None and quote < min_quote:
-                        min_quote = quote
-
-                # settings for chart dimensions and spacing
-                scale = 4
-                chart_width = max(388, num_quotes-1) * scale
-                chart_height = 150 * scale
-                chart_padding_top = 10 * scale
-                chart_padding_bottom = 10 * scale
-                chart_margin_top = 30 * scale
-                chart_margin_right = 70 * scale
-                chart_margin_bottom = 0 * scale
-                chart_margin_left = 0 * scale
-
-                # Create a canvas for the chart
-                canvas = draw.Drawing(
-                    chart_width + chart_margin_right + chart_margin_left,
-                    chart_height + chart_padding_top + chart_padding_bottom + chart_margin_top + chart_margin_bottom,
-                    displayInline=False)
-
-                # create a chart using a polygon
-                chart_ratio = (max_quote - min_quote) / chart_height
-                step_x = scale
-                start_x = chart_margin_left
-                start_y = chart_margin_bottom
-                offset_y = chart_margin_bottom + chart_padding_bottom
-                end_x = start_x + (num_quotes-1) * step_x
-                end_y = start_y
-                previous_close_y = ((previousClose - min_quote) / chart_ratio) + offset_y
-                last_y = ((quotes[num_quotes - 1] - min_quote) / chart_ratio) + offset_y
-
-                # create chart points
-                points = []
-                next_x = start_x - step_x
-                next_y = None
-                for idx in range(num_quotes):
-                    point = quotes[idx]
-                    if point is not None:
-                        next_y = ((point - min_quote) / chart_ratio) + offset_y
-                    next_x += step_x
-                    points.append(next_x)
-                    points.append(next_y)
-                points.append(end_x)
-                points.append(end_y)
-
-                # chart color
-                chart_fill = "rgba({green_rgb},0.5)".format_map({"green_rgb": "0,135,60"})
-                if (previous_close_y > last_y):
-                    chart_fill = "rgba({red_rgb},0.5)".format_map({"red_rgb": "240,22,47"})
-
-                # draw chart with white background
-                canvas.append(draw.Rectangle(
-                    start_x, chart_margin_bottom,
-                    chart_width, chart_height + chart_padding_top + chart_padding_bottom,
-                    fill="white"))
-                canvas.append(draw.Lines(
-                    chart_margin_left, chart_margin_bottom, *points,
-                    close=True, fill=chart_fill, stroke="black"))
-
-                # settings for creating tags and lines
-                sig_figs = len(str(round(max_quote)))
-                width_char = 14 * scale
-                font_size = 14 * scale
-                width_nubbin = width_char
-                width_rect = (width_char * (sig_figs + 2))
-                height_rect = 22 * scale
-                chart_boundary_x = start_x + chart_width
-
-                # create tag for previous close
-                prev_close_value = str(round(previousClose, 2))
-                prev_close_tag = self.create_chart_tag(prev_close_value, font_size,
-                                                       start_x, chart_boundary_x, previous_close_y, width_nubbin,
-                                                       width_rect, height_rect, "grey")
-                for el in prev_close_tag:
-                    canvas.append(el)
-
-                # create tag for current close
-                curr_close_value = str(round(quotes[num_quotes - 1], 2))
-                curr_close_tag = self.create_chart_tag(curr_close_value, font_size,
-                                                       start_x, chart_boundary_x, last_y, width_nubbin,
-                                                       width_rect, height_rect, chart_fill)
-                for el in curr_close_tag:
-                    canvas.append(el)
-
-                # convert svg to png
-                png_bytes = canvas.rasterize().pngData
-                
+                png_bytes = Chart(quotes, previousClose, scale=2).draw_chart()
                 file = discord.File(io.BytesIO(initial_bytes=png_bytes), filename="test.png")
                 return file
 
-    def create_chart_tag(self, value, font_size, start_x, end_x, start_y, width_nubbin, width_rect, height_rect, fill_color):
-        chart_line = draw.Line(start_x, start_y,
-                               end_x, start_y,
-                               stroke="black")
-        tag_rect = draw.Lines(end_x, start_y,
-                              end_x + width_nubbin, start_y + height_rect / 2,
-                              end_x + width_nubbin + width_rect, start_y + height_rect / 2,
-                              end_x + width_nubbin + width_rect, start_y - height_rect / 2,
-                              end_x + width_nubbin, start_y - height_rect / 2,
-                              close=True, fill=fill_color)
-        tag_text = draw.Text(value, font_size,
-                             end_x + width_nubbin, start_y - font_size * 0.3,
-                             fill="white", font_family="monospace", font_weight="bold")
-        return [chart_line, tag_rect, tag_text]
 
 def setup(client):
     client.add_cog(Stonk(client))
---
+++ bot/utils/stonkchart.py
@@ -0,0 +1,122 @@
+import drawSvg as draw
+
+class Chart:
+    def __init__(self,
+                 quotes, prev_close_quote,
+                 chart_width=388,
+                 chart_height=150,
+                 chart_padding_top=10,
+                 chart_padding_bottom=10,
+                 chart_margin_top=3,
+                 chart_margin_right=7,
+                 chart_margin_bottom=0,
+                 chart_margin_left=0,
+                 font_size=14,
+                 scale=1):
+        # so many settings
+        self.quotes = quotes
+        self.prev_close_quote = prev_close_quote
+        self.scale = scale
+        self.chart_width = max(chart_width, len(quotes)-1) * scale
+        self.chart_height = chart_height * scale
+        self.chart_padding_top = chart_padding_top * scale
+        self.chart_padding_bottom = chart_padding_bottom * scale
+        self.chart_margin_top = 30 * scale
+        self.chart_margin_right = 70 * scale
+        self.chart_margin_bottom = 0 * scale
+        self.chart_margin_left = 0 * scale
+        self.font_size = font_size * scale
+
+    def draw_chart(self):
+        # Create a canvas for the chart
+        canvas = draw.Drawing(
+            self.chart_width + self.chart_margin_right + self.chart_margin_left,
+            self.chart_height + self.chart_padding_top + self.chart_padding_bottom + self.chart_margin_top + self.chart_margin_bottom,
+            displayInline=False)
+
+        # create a chart using a polygon
+        max_quote = self.prev_close_quote
+        min_quote = self.prev_close_quote
+        for quote in self.quotes:
+            if quote is not None and quote > max_quote:
+                max_quote = quote
+            if quote is not None and quote < min_quote:
+                min_quote = quote
+        chart_ratio_y = (max_quote - min_quote) / self.chart_height
+        step_x = self.scale
+        start_x = self.chart_margin_left
+        start_y = self.chart_margin_bottom
+        offset_y = self.chart_margin_bottom + self.chart_padding_bottom
+        end_x = start_x + (len(self.quotes)-1) * step_x
+        end_y = start_y
+        previous_close_y = ((self.prev_close_quote - min_quote) / chart_ratio_y) + offset_y
+        last_y = ((self.quotes[len(self.quotes) - 1] - min_quote) / chart_ratio_y) + offset_y
+
+        # create chart points
+        points = []
+        next_x = start_x - step_x
+        next_y = None
+        for idx in range(len(self.quotes)):
+            point = self.quotes[idx]
+            if point is not None:
+                next_y = ((point - min_quote) / chart_ratio_y) + offset_y
+            next_x += step_x
+            points.append(next_x)
+            points.append(next_y)
+        points.append(end_x)
+        points.append(end_y)
+
+        # chart color
+        chart_fill = "rgba({green_rgb},0.5)".format_map({"green_rgb": "0,135,60"})
+        if (previous_close_y > last_y):
+            chart_fill = "rgba({red_rgb},0.5)".format_map({"red_rgb": "240,22,47"})
+
+        # draw chart with white background
+        canvas.append(draw.Rectangle(
+            start_x, self.chart_margin_bottom,
+            self.chart_width, self.chart_height + self.chart_padding_top + self.chart_padding_bottom,
+            fill="white"))
+        canvas.append(draw.Lines(
+            self.chart_margin_left, self.chart_margin_bottom, *points,
+            close=True, fill=chart_fill, stroke="black"))
+
+        # settings for creating tags and lines
+        prev_close_value = round(self.prev_close_quote, 2)
+        curr_close_value = round(self.quotes[len(self.quotes) - 1], 2)
+        sig_figs = len(str(max(prev_close_value, curr_close_value)))
+        self.width_tagrect = (self.font_size * (sig_figs - 1))
+        self.height_tagrect = self.font_size + (self.font_size / 2)
+        chart_boundary_x = start_x + self.chart_width
+
+        # create tag for previous close
+        prev_close_elements = self.create_tag_elements(prev_close_value, start_x, chart_boundary_x, previous_close_y, "grey")
+        for el in prev_close_elements:
+            canvas.append(el)
+
+        # create tag for current close
+        curr_close_elements = self.create_tag_elements(curr_close_value, start_x, chart_boundary_x, last_y, chart_fill)
+        for el in curr_close_elements:
+            canvas.append(el)
+
+        # convert svg to png
+        png_bytes = canvas.rasterize().pngData
+        return png_bytes
+
+    def create_tag_elements(self, value, start_x, end_x, start_y, fill_color):
+        # draw horizontal line across chart
+        chart_line = draw.Line(start_x, start_y,
+                               end_x, start_y,
+                               stroke="black")
+        # create tag polygon
+        width_nubbin = self.font_size
+        tag_rect = draw.Lines(end_x, start_y,
+                              end_x + width_nubbin, start_y + self.height_tagrect / 2,
+                              end_x + width_nubbin + self.width_tagrect, start_y + self.height_tagrect / 2,
+                              end_x + width_nubbin + self.width_tagrect, start_y - self.height_tagrect / 2,
+                              end_x + width_nubbin, start_y - self.height_tagrect / 2,
+                              close=True, fill=fill_color)
+        # create tag text
+        tag_text = draw.Text(str(value), self.font_size,
+                             end_x + width_nubbin, start_y - self.font_size * 0.3,
+                             fill="white", font_family="monospace", font_weight="bold")
+        return [chart_line, tag_rect, tag_text]
