c3ab82b3cda3e3888fbc074ee6ce753f6e44aa53
elements <126486+elements@users.noreply.github.com>
2021-02-28 00:06:48 +0000

    Updating Weather Formatting

--- bot/cogs/weather.py
+++ bot/cogs/weather.py
@@ -24,27 +24,14 @@ class Weather(commands.Cog):
             url = 'http://api.openweathermap.org/data/2.5/weather?'
             async with self.bot.session.get(url, params=payload, headers=headers) as r:
                 if r.status == 200:
-                    js = await r.json()
-                    celcius = js['main']['temp'] - 273
-                    fahrenheit = js['main']['temp'] * 9 / 5 - 459
-                    temperature = '{0:.1f} Celsius\n{1:.1f} Fahrenheit'.format(celcius, fahrenheit)
-                    humidity = str(js['main']['humidity']) + '%'
-                    pressure = str(js['main']['pressure']) + ' hPa'
-                    wind_kmh = str(round(js['wind']['speed'] * 3.6)) + ' km/h'
-                    wind_mph = str(round(js['wind']['speed'] * 2.23694)) + ' mph'
-                    clouds = js['weather'][0]['description'].title()
-                    icon = js['weather'][0]['icon']
-                    name = js['name'] + ', ' + js['sys']['country']
-                    city_id = js['id']
+                    weather_response = await r.json()
+                    icon = weather_response['weather'][0]['icon']
+                    name = weather_response['name'] + ', ' + weather_response['sys']['country']
+                    city_id = weather_response['id']
                     em = discord.Embed(title='Weather in {}'.format(name), color=discord.Color.blue(),
-                                       description='\a\n',
+                                       description=js['weather'][0]['description'],
                                        url='https://openweathermap.org/city/{}'.format(city_id))
-                    em.add_field(name='**Conditions**', value=clouds)
-                    em.add_field(name='**Temperature**', value=temperature)
-                    em.add_field(name='\a', value='\a')
-                    em.add_field(name='**Wind**', value='{}\n{}'.format(wind_kmh, wind_mph))
-                    em.add_field(name='**Pressure**', value=pressure)
-                    em.add_field(name='**Humidity**', value=humidity)
+                    embed.add_field(name='Weather', value=f"**🌡️ Current Temp:** {weather_response['main']['temp']}\n**🌡️ Feels Like:** {weather_response['main']['feels_like']}\n**🌡️ Daily High:** {weather_response['main']['temp_max']}\n**🌡️ Daily Low:** {weather_response['main']['temp_min']}\n**Humidity:** {weather_response['main']['humidity']}%\n**🌬️ Wind:** {weather_response['wind']['speed']} mph", inline=False)
                     em.set_thumbnail(url='https://openweathermap.org/img/w/{}.png'.format(icon))
                     await ctx.send(embed=em)
 
